2971b9f3ff8f4235563fc0be13bd88615d4a237b
Pawan rai <pawanvirat32@gmail.com>
2026-04-15 00:01:39 +0530

    Add log files and compiled Python files for data ingestion process
    - Created log files capturing the data ingestion process, including thread usage and error messages.
    - Added compiled Python files for the `__init__`, `exception`, `logger`, and `utils` modules to the cache.
    - Implemented a utility function to save Python objects using pickle in the `utils` module.

---
+++ src/utils.py
@@ -0,0 +1,23 @@
+import os
+import sys
+import pickle
+from src.exception import CustomException
+
+
+def save_object(file_path, obj):
+    """
+    Save a Python object to a file using pickle.
+    
+    Args:
+        file_path: Path where the object should be saved
+        obj: Object to be saved
+    """
+    try:
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path, exist_ok=True)
+        
+        with open(file_path, 'wb') as file_obj:
+            pickle.dump(obj, file_obj)
+            
+    except Exception as e:
+        raise CustomException(e, sys)
